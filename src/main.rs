4b4e9155aa9fa7abab6a665485a58189f4e2786f
aydenhex <perso.ayden@gmail.com>
2023-04-30 18:58:33 +0200

    add cors + .env handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use axum::{routing::get, Router};
+use dotenv::dotenv;
+use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    //Load env var
+    dotenv().ok();
     // Init tracing
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
@@ -11,10 +15,13 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new().route("/", get(|| async { "hello, world" }));
+    let cors = CorsLayer::new().allow_origin(Any);
+
+    let app = Router::new()
+        .route("/", get(|| async { "hello, world" }))
+        .layer(cors);
 
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
-    std::println!("coucou");
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
