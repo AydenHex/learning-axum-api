fb9a8111fb9a0dff6bc132cd8aaaba5362b8bd6b
aydenhex <perso.ayden@gmail.com>
2023-04-30 19:16:17 +0200

    add database connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use axum::{routing::get, Router};
+use axum::{extract::Extension, routing::get, Router};
 use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -7,6 +8,16 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 async fn main() {
     //Load env var
     dotenv().ok();
+
+    let database_url = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        std::env::var("DATABASE_USER").expect("set DATABASE_USER env variable"),
+        std::env::var("DATABASE_PASSWORD").expect("set DATABASE_PASSWORD env variable"),
+        std::env::var("DATABASE_HOST").expect("set DATABASE_HOST env variable"),
+        std::env::var("DATABASE_PORT").expect("set DATABASE_PORT env variable"),
+        std::env::var("DATABASE_NAME").expect("set DATABASE_NAME env variable"),
+    );
+
     // Init tracing
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
@@ -17,9 +28,16 @@ async fn main() {
 
     let cors = CorsLayer::new().allow_origin(Any);
 
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Can't connect to database");
+
     let app = Router::new()
         .route("/", get(|| async { "hello, world" }))
-        .layer(cors);
+        .layer(cors)
+        .layer(Extension(pool));
 
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
